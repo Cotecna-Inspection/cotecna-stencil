8c3711a656d0108c00981beb8e351ab508c5883d
Pujol Marcel <marcel.pujol@cotecna.es>
2022-08-09 10:13:38 +0200

    adding multidropdown component

--- src/components/multidropdown/multidropdown.tsx
+++ src/components/multidropdown/multidropdown.tsx
@@ -1,32 +1,55 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
-
+import { Component, Prop, h, Event, EventEmitter, State, Listen } from '@stencil/core';
+import { FieldMode } from '../../enums/fieldMode';
+import { ControlState } from '../../models/controlState';
+import { Field } from '../../models/field';
+import { isValid } from '../../utils/field-utils';
 @Component({
-  tag: 'my-component',
-  styleUrl: 'my-component.css',
-  shadow: true,
+  tag: 'cotecna-multidropdown',
+  styleUrl: 'multidropdown.scss',
+  shadow: true
 })
-export class MyComponent {
-  /**
-   * The first name
-   */
-  @Prop() first: string;
+export class Multidropdown {
 
-  /**
-   * The middle name
-   */
-  @Prop() middle: string;
+  @Prop()
+  field!: Field;
 
-  /**
-   * The last name
-   */
-  @Prop() last: string;
+  @Prop()
+  control!: any;
 
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
+  @Event()
+  change: EventEmitter<ControlState>;
+
+  @State() private mode: FieldMode = FieldMode.READ;
+
+  @Listen('editionFinished')
+  onEditionFinished(event: CustomEvent<{field: Field, isChanged: boolean}>) {
+    this.field = {...event.detail.field};
+    if (event.detail.isChanged) this.onChange();
+    this.mode = FieldMode.READ;
   }
 
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return (
+      <div class="multidropdown-container" onClick={this.setEditionMode.bind(this)}>
+        <cotecna-multidropdown-readable field={this.field}/>
+        { 
+          this.mode === FieldMode.EDITION 
+            ? <cotecna-multidropdown-editable field={this.field} control={this.control} />
+            : null 
+        }
+      </div>
+    )
+  }
+
+  private setEditionMode() {
+    this.mode = FieldMode.EDITION;
+  }
+
+  private onChange() {
+    this.change.emit({
+      isValid: isValid(this.field),
+      value: this.field.value,
+      readableValue: this.field.readableValue
+    } as ControlState)
   }
 }
