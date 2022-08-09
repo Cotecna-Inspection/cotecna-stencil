04a88e0de1fa73295c990e629bfb82eb046657cc
Pujol Marcel <marcel.pujol@cotecna.es>
2022-08-09 10:15:48 +0200

    components changes

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,57 +5,100 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
+import { DialogAction } from "./enums/dialogAction";
+import { Field } from "./models/field";
+import { ControlState } from "./models/controlState";
 export namespace Components {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first": string;
-        /**
-          * The last name
-         */
-        "last": string;
-        /**
-          * The middle name
-         */
-        "middle": string;
+    interface CotecnaDialog {
+    }
+    interface CotecnaMultidropdown {
+        "control": any;
+        "field": Field;
+    }
+    interface CotecnaMultidropdownEditable {
+        "control": any;
+        "field": Field;
+    }
+    interface CotecnaMultidropdownReadable {
+        "field": Field;
     }
 }
+export interface CotecnaDialogCustomEvent<T> extends CustomEvent<T> {
+    detail: T;
+    target: HTMLCotecnaDialogElement;
+}
+export interface CotecnaMultidropdownCustomEvent<T> extends CustomEvent<T> {
+    detail: T;
+    target: HTMLCotecnaMultidropdownElement;
+}
+export interface CotecnaMultidropdownEditableCustomEvent<T> extends CustomEvent<T> {
+    detail: T;
+    target: HTMLCotecnaMultidropdownEditableElement;
+}
 declare global {
-    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
+    interface HTMLCotecnaDialogElement extends Components.CotecnaDialog, HTMLStencilElement {
     }
-    var HTMLMyComponentElement: {
-        prototype: HTMLMyComponentElement;
-        new (): HTMLMyComponentElement;
+    var HTMLCotecnaDialogElement: {
+        prototype: HTMLCotecnaDialogElement;
+        new (): HTMLCotecnaDialogElement;
+    };
+    interface HTMLCotecnaMultidropdownElement extends Components.CotecnaMultidropdown, HTMLStencilElement {
+    }
+    var HTMLCotecnaMultidropdownElement: {
+        prototype: HTMLCotecnaMultidropdownElement;
+        new (): HTMLCotecnaMultidropdownElement;
+    };
+    interface HTMLCotecnaMultidropdownEditableElement extends Components.CotecnaMultidropdownEditable, HTMLStencilElement {
+    }
+    var HTMLCotecnaMultidropdownEditableElement: {
+        prototype: HTMLCotecnaMultidropdownEditableElement;
+        new (): HTMLCotecnaMultidropdownEditableElement;
+    };
+    interface HTMLCotecnaMultidropdownReadableElement extends Components.CotecnaMultidropdownReadable, HTMLStencilElement {
+    }
+    var HTMLCotecnaMultidropdownReadableElement: {
+        prototype: HTMLCotecnaMultidropdownReadableElement;
+        new (): HTMLCotecnaMultidropdownReadableElement;
     };
     interface HTMLElementTagNameMap {
-        "my-component": HTMLMyComponentElement;
+        "cotecna-dialog": HTMLCotecnaDialogElement;
+        "cotecna-multidropdown": HTMLCotecnaMultidropdownElement;
+        "cotecna-multidropdown-editable": HTMLCotecnaMultidropdownEditableElement;
+        "cotecna-multidropdown-readable": HTMLCotecnaMultidropdownReadableElement;
     }
 }
 declare namespace LocalJSX {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first"?: string;
-        /**
-          * The last name
-         */
-        "last"?: string;
-        /**
-          * The middle name
-         */
-        "middle"?: string;
+    interface CotecnaDialog {
+        "onClickDialogAction"?: (event: CotecnaDialogCustomEvent<DialogAction>) => void;
+    }
+    interface CotecnaMultidropdown {
+        "control": any;
+        "field": Field;
+        "onChange"?: (event: CotecnaMultidropdownCustomEvent<ControlState>) => void;
+    }
+    interface CotecnaMultidropdownEditable {
+        "control": any;
+        "field": Field;
+        "onEditionFinished"?: (event: CotecnaMultidropdownEditableCustomEvent<{ field: Field, isChanged: boolean}>) => void;
+    }
+    interface CotecnaMultidropdownReadable {
+        "field": Field;
     }
     interface IntrinsicElements {
-        "my-component": MyComponent;
+        "cotecna-dialog": CotecnaDialog;
+        "cotecna-multidropdown": CotecnaMultidropdown;
+        "cotecna-multidropdown-editable": CotecnaMultidropdownEditable;
+        "cotecna-multidropdown-readable": CotecnaMultidropdownReadable;
     }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "cotecna-dialog": LocalJSX.CotecnaDialog & JSXBase.HTMLAttributes<HTMLCotecnaDialogElement>;
+            "cotecna-multidropdown": LocalJSX.CotecnaMultidropdown & JSXBase.HTMLAttributes<HTMLCotecnaMultidropdownElement>;
+            "cotecna-multidropdown-editable": LocalJSX.CotecnaMultidropdownEditable & JSXBase.HTMLAttributes<HTMLCotecnaMultidropdownEditableElement>;
+            "cotecna-multidropdown-readable": LocalJSX.CotecnaMultidropdownReadable & JSXBase.HTMLAttributes<HTMLCotecnaMultidropdownReadableElement>;
         }
     }
 }
