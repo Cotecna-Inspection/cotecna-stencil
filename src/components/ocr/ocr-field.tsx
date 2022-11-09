baf622c43a19b14af60f0a1a988ad537841fd865
Pujol Marcel <marcel.pujol@cotecna.es>
2022-11-09 14:26:12 +0100

    adding ocr component

--- src/components/ocr/ocr-field.tsx
+++ src/components/ocr/ocr-field.tsx
@@ -1,45 +1,87 @@
-import { Component, h, State } from '@stencil/core';
+import { Component, EventEmitter, h, Prop, State, Event } from '@stencil/core';
+import { Field } from '../../models/field';
+import { getIconPNGPath, getSymbol, isValid } from '../../utils/field-utils';
 import { OCRResult } from './models/ocr-result.model';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { ControlState } from '../../models/controlState';
 declare var mltext;
-declare var navigator;
 
+/** @internal **/
 @Component({
   tag: 'cotecna-ocr',
   styleUrl: 'ocr-field.scss',
   shadow: true,
 })
 export class OcrField {
+  @Prop()
+  public field!: Field;
+
+  @Prop()
+  public control!: any;
+
   @State()
   private ocrResult: OCRResult = null;
 
+  @State()
+  private ocrResultAsString: string = null;
+
+  @Event()
+  public fieldChange: EventEmitter<ControlState>;
+
   render() {
     return (
-      <div class="ocr-field-container">
-        { this.ocrResult?.foundText ? this.getOcrResult() : '' }
-        <button onClick={() => this.handleGeneration()}>OCR</button>
+      <div class="ocr-field-container" part="container">
+        { this.getFieldLabel() }
+        { this.getFieldContainer() }
       </div>
     );
   }
 
-  private getOcrResult(): void {
+  private getFieldLabel(): any {
     return (
-      <div>
-        { this.ocrResult?.lines?.lineText.map(text => <p>{text}</p>) }
-      </div>
-    );
+      <div class="label-container">
+          <label part="label">
+            {this.field.label}
+            {getSymbol(this.field)}
+          </label>
+        </div>
+    )
   }
 
-  private handleGeneration(): void {
-    document.addEventListener("deviceready", () => {
-      navigator.camera.getPicture(
-        (imageData) => {
-          this.performOCR(imageData);
-        },
-        (err) => {
-          console.error('err', err);
-        },
-        { quality: 100, correctOrientation: true });
-    });
+  private getFieldContainer(): any {
+    return (
+      <div class={{"field-container": true, 'invalid-field': !isValid(this.field)}}>
+        <textarea id={`ocr-result-${this.field.id}`}
+          rows={4} cols={50} 
+          value={this.field.value}
+          onChange={(e) => this.onChangeOcrResult(e)}>
+        </textarea>
+        <button onClick={() => this.takePictureAndPerformOcr()}><img src={getIconPNGPath('photo_camera')}></img></button>
+        <button onClick={() => this.deleteOcrResult()}><img src={getIconPNGPath('delete')}></img></button>
+      </div>
+    )
+  }
+
+  private onChangeOcrResult(event: Event): void {
+    const target: any = event.target;
+    this.ocrResultAsString = (target.value !== '') ? target.value : null;
+    this.onChange();
+  }
+
+  private async takePictureAndPerformOcr(): Promise<void> {
+    try {
+      const image = await Camera.getPhoto({ quality: 100, allowEditing: false, resultType: CameraResultType.Uri });
+      this.performOCR(image.path);
+    } catch(err) {
+      console.error('take picture and perform ocr', err);
+      throw err;
+    }
+  }
+
+  private deleteOcrResult(): void {
+    this.ocrResult = null;
+    this.ocrResultAsString = null;
+    this.onChange();
   }
   
   private performOCR(imageData: any): void {
@@ -47,6 +89,8 @@ export class OcrField {
     mltext.getText((result: any) => {
       if (result.foundText) {
         this.ocrResult = this.mapToOcrResult(result);
+        this.setOcrResultAsString();
+        this.onChange();
       }
     }, (err) => {
       console.error('error', err);
@@ -59,5 +103,17 @@ export class OcrField {
       lines: { lineText: result.lines.linetext },
       words: { wordText: result.words.wordtext }
     } as OCRResult;
+  }
+
+  private setOcrResultAsString(): void {
+    this.ocrResultAsString = this.ocrResult?.lines?.lineText.map(text => `${text}\n`)?.toString();
+  }
+
+  private onChange(): void {
+    this.field.value = this.ocrResultAsString;
+    this.fieldChange.emit({
+      isValid: isValid(this.field),
+      value: this.field.value
+    })
   }
 }
