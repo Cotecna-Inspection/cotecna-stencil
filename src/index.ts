f6c52494ce947c468d72f5b960b5a6e5c7f6d482
Pujol Marcel <marcel.pujol@cotecna.es>
2022-11-09 14:26:32 +0100

    adding ocr configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { configEmailBox } from "./integration-test/emailbox-integration";
 import { configMultiDropdown } from "./integration-test/multidropdown-integration";
+import { configOcr } from './integration-test/ocr-integration';
 import { configObjectCounter } from "./integration-test/object-counter-integration";
 
-configEmailBox();
+defineCustomElements(window);
+
 configMultiDropdown();
+configEmailBox();
+configOcr();
 configObjectCounter();
 
 document.addEventListener('fieldChange', (event: any) => {
-  console.log('fieldChange.event', event.detail.value);
+  console.log('fieldChange.event', event.detail);
 });
+
