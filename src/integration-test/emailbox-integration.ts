a231124fbbb7961d39032c5f21638033e2184b05
Pujol Marcel <marcel.pujol@cotecna.es>
2022-11-09 14:35:51 +0100

    fix integration

--- src/integration-test/emailbox-integration.ts
+++ src/integration-test/emailbox-integration.ts
@@ -1,27 +1,37 @@
 import { ControlType } from "../enums/controlType";
+import { EMAIL_BOX_FIELD } from "../test-data/email-box-field.mocked";
 
 export function configEmailBox() {
-    var emailBoxField: any = document.querySelector('cotecna-email-box');
-  
-    emailBoxField.field = {
-      id: '2',
-      type: ControlType.EmailBox,
-      label: 'Share by Email',
-      order: 2,
-      propertyName: 'share-by-email',
-      value: [],
-      readableValue: [],
-      required: false,
-      visible: true,
-      valid: false,
-      readOnly: false
-    };
-  
-    emailBoxField.control = {
-      defaultEmails: [
-        'marcel.pujol@cotecna.es',
-        'carles.romo@cotecna.es',
-        'eltjo.kraai@cotecna.es'
-      ]
-    };
-  }
+  var emailBoxField: any = document.querySelector("cotecna-email-box");
+
+  emailBoxField.field = {
+    id: "2",
+    type: ControlType.EmailBox,
+    label: "Share by Email",
+    order: 2,
+    propertyName: "share-by-email",
+    value: [],
+    readableValue: [],
+    required: false,
+    visible: true,
+    valid: false,
+    readOnly: false,
+  };
+
+  emailBoxField.control = {
+    defaultEmails: [
+      "marcel.pujol@cotecna.es",
+      "carles.romo@cotecna.es",
+      "eltjo.kraai@cotecna.es",
+    ],
+  };
+}
+
+export function configEmailBoxEvents() {
+  const emailBoxCheckbox = document.getElementById('email-box-checkbox');
+  const emailBoxField: any = document.querySelector('cotecna-email-box');
+  emailBoxCheckbox?.addEventListener('change', (event: any) => {  
+    const readonlyStatus = event.currentTarget.checked;
+    emailBoxField.field = Object.assign({}, EMAIL_BOX_FIELD, { readOnly: readonlyStatus });
+  }, false);
+}
