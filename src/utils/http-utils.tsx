14973c4d9c0a7ed78e502cb1cb644b0a5a1f3506
RocioDelJ <rocioodelgado@outlook.com>
2023-06-12 13:32:58 +0200

    added basic auth to object counter

--- src/utils/http-utils.tsx
+++ src/utils/http-utils.tsx
@@ -2,6 +2,7 @@ export function postMultipartFormData(url: string, formData: FormData): Promise<
   return new Promise<void>((resolve, reject) => {
     const request = new XMLHttpRequest();
     request.open('POST', url);
+    request.setRequestHeader('Authorization', 'cotecnaAIObjectCounter:ded41b3c-8a1e-4296-b7fa-542850c44909');
     request.send(formData);
     request.onreadystatechange = () => {
       if (request.readyState === 4) {
